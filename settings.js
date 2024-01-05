d5d9ee0398f243ae4b48dd6716ea47305d1d7f92
TomSer834 <tom.mailing@gmail.com>
2024-01-05 06:05:39 +0100

    Initial commit

---
+++ settings.js
@@ -0,0 +1,47 @@
+/* columns settings */
+columnTitles = [
+    "Length [m]",
+    "MacClain et al.",
+    "Gottfried et al.",
+    "Kohler et al.",
+    "Tricas & McCosker",
+    "Mollet & Cailliet",
+    "Casey & Pratt",
+    "Average"
+];
+
+columnTitleColors = [
+    "#111111",
+    "333333",
+    "333333",
+    "333333",
+    "333333",
+    "333333",
+    "333333",
+    "111111"
+];
+
+columnColors = [
+    "111111",
+    "500505",
+    "553000",
+    "204505",
+    "003842",
+    "002060",
+    "400540",
+    "111111"
+];
+
+/* cell rows settings */
+let legend = [
+    "Weight calculated using the authors' equations",
+    "Weight calculated using the square-cube law, based on the reference",
+    "Difference (1) minus (2) = allometric effect",
+    "Allometric addition to (2)"
+];
+
+let units = ["kg", "kg", "kg", "%"];
+
+let addPlus = [false, false, true, true];
+
+let roundToDigits = [0, 0, 0, 1];
